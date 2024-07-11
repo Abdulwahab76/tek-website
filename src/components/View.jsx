ddf55049e0975d9ef0c435c1dbdaa8347d7ec79b
Abdulwahab76 <abdulwahab12.pk@gmail.com>
2024-07-11 19:34:06 +0500

    add layout css

---
+++ src/components/View.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const View = ({ children }) => {
+  return <div className="max-w-7xl mx-auto w-full">{children}</div>;
+};
+
+export default View;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import View from "./View";
+import Card from "./Card";
+export {Navbar,Footer,View,Card}
